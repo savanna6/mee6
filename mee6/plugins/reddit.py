6684f0beac9ae9f6792997b6f05ed060eb7e37bb
cookkkie <cookie.io@openmailbox.org>
2017-05-12 09:06:12 +0200

    Stay bellow RL when sending messages

--- mee6/plugins/reddit.py
+++ mee6/plugins/reddit.py
@@ -1,4 +1,5 @@
 import gevent
+import gevent.queue
 import requests
 
 from collections import defaultdict
@@ -12,12 +13,34 @@ MESSAGE_FORMAT = "`New post from /r/{subreddit}`\n\n"\
                  "**{title}** *by {author}*\n"\
                  "**Link** {link}\n"\
                  "**Thread** {thread} \n\n"
+SENDER_PACE = 0.1
+SUBREDDITS_BATCH_SIZE = 50
 
 class Reddit(Plugin):
 
     id = "reddit"
     name = "Reddit"
     description = "Get posts from your favourite subreddits directly to your Discord server"
+
+    sender_queue = gevent.queue.Queue()
+    sender_worker_instance = None
+
+    def sender_worker(self, wait_time):
+        while True:
+            channel_id, message, guild, num = self.sender_queue.get()
+            try:
+                send_message(channel_id, message)
+                self.log('Sent {} posts to {}\'s channel #{}'.format(num,
+                                                                     guild,
+                                                                     channel_id))
+            except APIException as e:
+                self.log('An exception occured sending message in'\
+                         '{} // {}'.format(guild.id, e.payload))
+            except Exception as e:
+                self.log('An unknown expection occured sending message...'\
+                         '{}'.format(e))
+            finally:
+                gevent.sleep(wait_time)
 
     def get_config(self, guild):
         config = PluginConfig()
@@ -27,7 +50,7 @@ class Reddit(Plugin):
     def announce(self, posts, guild):
         to_announce = []
         for post in posts:
-            if guild.storage.sismember('announced', post['id']): continue
+            #if guild.storage.sismember('announced', post['id']): continue
             to_announce.append(post)
 
         messages = []
@@ -53,19 +76,15 @@ class Reddit(Plugin):
 
         display_channel_id = guild.storage.get('display_channel') or guild.id
         for message, posts_ids in zip(messages, posts_ids):
-            try:
-                message = send_message(display_channel_id, message)
-            except APIException as e:
-                self.log('An exception occured sending message in'\
-                         '{} // {}'.format(guild.id, e.payload))
-                continue
-
+            self.sender_queue.put((display_channel_id, message, guild,
+                                   len(posts_ids)))
             guild.storage.sadd('announced', *posts_ids)
-            self.log('Sent {} posts to {}\'s channel #{}'.format(len(posts_ids),
-                                                                       guild,
-                                                                       display_channel_id))
 
     def loop(self):
+        if self.sender_worker_instance is None:
+            self.sender_worker_instance = gevent.spawn(self.sender_worker,
+                                                       SENDER_PACE)
+
         subreddits = self.time_log('Fetching subreddits list',
                                    self.db.smembers,
                                    'Reddit.#:subs')
@@ -79,14 +98,14 @@ class Reddit(Plugin):
                 subreddits_map[subreddit].append(guild)
 
         subreddits_posts = {}
-        for subs in chunk(subreddits, 100):
+        for subs in chunk(subreddits, SUBREDDITS_BATCH_SIZE):
             posts = self.time_log('Getting {} posts'.format('+'.join(subs)),
                                   self.get_posts, subs)
             gevent.sleep(1)
             for subreddit, subreddit_posts in posts.items():
-                guilds = subreddits_map.get(subreddit)
+                guilds = subreddits_map.get(subreddit, [])
                 for guild in guilds:
-                    gevent.spawn(self.announce, subreddit_posts, guild)
+                    gevent.spawn(self.announce, list(reversed(subreddit_posts)), guild)
 
     def get_posts(self, subreddits):
         url ='https://www.reddit.com/r/{subs}/new.json?limit=100'.format(subs='+'.join(subreddits))
