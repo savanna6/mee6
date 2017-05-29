02c5b2a476f54d64283af0bae299c1f3861f4451
cookkkie <cookie.io@openmailbox.org>
2017-05-29 03:42:05 +0200

    Add Exceptions file

---
+++ mee6/exceptions.py
@@ -0,0 +1,18 @@
+from json.decoder import JSONDecodeError
+
+class APIException(Exception):
+    def __init__(self, r):
+        msg = 'Request failed status_code={} payload={}'.format(r.status_code,
+                                                                r.text)
+        try:
+            decoded_payload = r.json()
+        except JSONDecodeError:
+            decoded_payload = {}
+
+        self.payload = r.text
+        self.status_code = r.status_code
+        self.error_code = decoded_payload.get('code')
+        self.error_message = decoded_payload.get('message')
+
+        super(APIException, self).__init__(msg)
+
