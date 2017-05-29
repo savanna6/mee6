1cb7443a030cc95bcf1411ee56d84af70017648c
cookkkie <cookie.io@openmailbox.org>
2017-05-29 03:42:58 +0200

    Add webhook type

--- mee6/types/__init__.py
+++ mee6/types/__init__.py
@@ -2,3 +2,4 @@ from mee6.types.channel import *
 from mee6.types.role import *
 from mee6.types.guild import *
 from mee6.types.message import *
+from mee6.types.webhook import *
---
+++ mee6/types/webhook.py
@@ -0,0 +1,8 @@
+class Webhook:
+    def __init__(self, **kwargs):
+        self.id = kwargs.get('id')
+        self.guild_id = kwargs.get('guild_id')
+        self.channel_id = kwargs.get('channel_id')
+        self.name = kwargs.get('name')
+        self.avatar = kwargs.get('avatar')
+        self.token = kwargs.get('token')
