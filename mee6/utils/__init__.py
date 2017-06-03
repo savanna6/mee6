f19d57c9e43557201be2f4907664289792a61f30
cookkkie <cookie.io@openmailbox.org>
2017-06-04 01:21:53 +0200

    Add timed feature

--- mee6/utils/__init__.py
+++ mee6/utils/__init__.py
@@ -1,9 +1,10 @@
+import os
+
 from mee6.utils.logger import Logger
 
 def chunk(l, n):
     l = list(l)
     return [l[i:i+n] for i in range(0, len(l), n)]
-
 
 import string
 digs = string.digits + string.ascii_letters
@@ -25,3 +26,24 @@ def get(obj, attr, default=None):
         return getattr(obj, attr)
     except AttributeError:
         return default
+
+from contextlib import contextmanager
+from time import time
+from datadog import statsd
+@contextmanager
+def timed(metric, tags={}):
+    start = time()
+    try:
+        yield
+    except Exception as e:
+        raise e
+    finally:
+        now = time()
+        tags = ['{}:{}'.format(tag_name, value) for tag_name, value in tags.items()]
+        statsd.timing(metric, (now - start) * 1000, tags=tags)
+
+def init_dd_agent():
+    dd_agent = os.getenv('DD_AGENT')
+    if dd_agent:
+        from datadog import initialize
+        initialize(statsd_host=dd_agent, statsd_port=8125)
