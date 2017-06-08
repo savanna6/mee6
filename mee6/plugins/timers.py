18b9df10f8f57fec897d28c90d854416861ef4e3
cookkkie <cookie.io@openmailbox.org>
2017-06-08 12:32:37 +0200

    Remove last_post_id check (thanks Sil)

--- mee6/plugins/timers.py
+++ mee6/plugins/timers.py
@@ -1,5 +1,5 @@
 from mee6 import Plugin
-from mee6.discord import get_channel_messages, send_message
+from mee6.discord import get_channel_messages, send_message, get_current_user
 from time import time
 
 import hashlib
@@ -14,6 +14,8 @@ class Timers(Plugin):
     description = "Send messages at specific intervals"
 
     timers_ids = {}
+
+    me = get_current_user()
 
     def get_default_config(self, guild_id):
         default_config = {'timers': []}
@@ -32,8 +34,8 @@ class Timers(Plugin):
         return [timer for guild in self.get_guilds() for timer in guild.config['timers']]
 
     def get_timer_id(self, timer_message, timer_interval, channel):
-        """ since storing timer ids on config is meh, i'm just ising a hash.
-        two identicall timers will collision. But we don't care here."""
+        """ since storing timer ids on config is meh, i'm just using a hash.
+        two identical timers will collision. But we don't care here."""
 
         phrase = '{}.{}.{}'.format(timer_message, timer_interval, channel)
         timer_id = self.timers_ids.get(phrase)
@@ -51,25 +53,20 @@ class Timers(Plugin):
         interval = timer['interval']
         timer_id = self.get_timer_id(message, interval, channel)
 
-        last_post = self.db.get('plugin.timers.{}.last_post'.format(timer_id))
-        if last_post:
-            last_post_id, last_post_timestamp = [int(v) for v in last_post.split(':')]
-        else:
-            last_post_id, last_post_timestamp = None, 0
+        last_post_timestamp = self.db.get('plugin.timers.{}.timestamp'.format(timer_id))
+        last_post_timestamp = int(last_post_timestamp or 0)
 
         if last_post_timestamp + timer['interval'] > now:
             return
 
-        if last_post_id:
-            last_messages = get_channel_messages(channel, limit=1)
-            if len(last_messages) > 0:
-                last_message = last_messages[-1]
-                if int(last_message.id) == last_post_id:
-                    return
+        last_messages = get_channel_messages(channel, limit=1)
+        if len(last_messages) > 0:
+            last_message = last_messages[-1]
+            if last_message.author.id == self.me.id:
+                return
 
         post_message = send_message(channel, message)
         self.log('Announcing timer message ({} interval) in {}'.format(interval, channel))
 
         now = math.floor(time())
-        last_post = '{}:{}'.format(post_message.id, now)
-        self.db.set('plugin.timers.{}.last_post'.format(timer_id), last_post)
+        self.db.set('plugin.timers.{}.last_post_timestamp'.format(timer_id), now)
