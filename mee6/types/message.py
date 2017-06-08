b5d7a76712eb36d828ed6b8a7a8f19428f503342
cookkkie <cookie.io@openmailbox.org>
2017-06-08 12:31:29 +0200

    Casting author to User type in Message

--- mee6/types/message.py
+++ mee6/types/message.py
@@ -2,7 +2,6 @@ class Message:
     def __init__(self, **kwargs):
         self.id = kwargs.get('id')
         self.channel_id = kwargs.get('channel_id')
-        self.author = kwargs.get('author')
         self.content = kwargs.get('content')
         self.timestamp = kwargs.get('timestamp')
         self.edited_timestamp = kwargs.get('edited_timestamp')
@@ -11,3 +10,8 @@ class Message:
         self.mention_everyone = kwargs.get('mention_everyone')
         self.mention_roles = kwargs.get('mention_roles')
         self.webhook_id = kwargs.get('webhook_id')
+
+        if not self.webhook_id:
+            self.author = User(**kwargs.get('author'))
+        else:
+            self.author = None
