9a8d13b8253b1ea7a2269de8f490dc4af4fd8754
cookkkie <cookie.io@openmailbox.org>
2017-06-11 22:15:21 +0200

    Add lock to ratellimit

--- mee6/discord/api/ratelimit.py
+++ mee6/discord/api/ratelimit.py
@@ -4,8 +4,13 @@ import time
 import math
 
 from mee6.utils import Logger
+from gevent.lock import Semaphore
 
 class Ratelimit(Logger):
+
+    def __init__(self):
+        self._lock = Semaphore()
+
     def get_route(route):
         raise NotImplemented
 
@@ -16,32 +21,38 @@ class Ratelimit(Logger):
         raise NotImplemented
 
     def check(self, route):
-        reset = self.get_route(route) or self.get_route('global')
+        with self._lock:
+            reset = self.get_route(route) or self.get_route('global')
+
         if not reset: return
 
         sleep_time = math.floor(max(0, reset - time.time()))
         self.log('Bucket {} full, waiting {}s'.format(route, sleep_time))
         gevent.sleep(sleep_time + 0.1)
 
-        self.del_route(route)
-        self.del_route('global')
+        with self._lock:
+            self.del_route(route)
+            self.del_route('global')
 
         return
 
     def update(self, route, r):
         remaining = r.headers.get('X-RateLimit-Remaining')
         if remaining == '0':
-            self.set_route(route, int(r.headers.get('X-RateLimit-Reset', 0)))
+            with self._lock:
+                self.set_route(route, int(r.headers.get('X-RateLimit-Reset', 0)))
         return
 
     def handle_429(self, route, r):
         payload = r.json()
         retry_after = math.ceil(payload['retry_after'] / 1000.)
         if payload['global']:
-            self.set_route('global', math.ceil(time.time() + retry_after))
+            with self._lock:
+                self.set_route('global', math.ceil(time.time() + retry_after))
             self.log('Received 429: Bucket {} [GLOBAL RATELIMIT], waiting {}s'.format(route, retry_after))
         else:
-            self.set_route(route, math.ceil(time.time() + retry_after))
+            with self._lock:
+                self.set_route(route, math.ceil(time.time() + retry_after))
             self.log('Received 429: Bucket {} full, waiting {}s'.format(route,
                                                                        retry_after))
         return retry_after
@@ -49,6 +60,7 @@ class Ratelimit(Logger):
 class LocalRatelimit(Ratelimit):
     def __init__(self):
         self.routes = {}
+        super(LocalRatelimit, self).__init__()
 
     def get_route(self, route):
         return self.routes.get(route)
@@ -62,6 +74,7 @@ class LocalRatelimit(Ratelimit):
 class RedisRatelimit(Ratelimit):
     def __init__(self, redis_url):
         self.r = redis.from_url(redis_url, decode_responses=True)
+        super(RedisRatelimit, self).__init__()
 
     def get_route(self, route):
         reset = self.r.get('Ratelimit.{}'.format(route))
