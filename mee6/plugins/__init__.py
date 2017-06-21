f018782201d3ece7a5b6980fc67f2271ab00d7c0
cookkkie <cookie.io@openmailbox.org>
2017-06-21 11:59:13 +0200

    Add music plugin import

--- mee6/plugins/__init__.py
+++ mee6/plugins/__init__.py
@@ -2,4 +2,4 @@ from mee6.plugins.reddit import Reddit
 from mee6.plugins.streamers import Streamers
 from mee6.plugins.timers import Timers
 #from mee6.plugins.dummy import Dummy
-#from mee6.plugins.music import Music
+from mee6.plugins.music import Music
